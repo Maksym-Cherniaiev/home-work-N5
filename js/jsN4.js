aa0b3d9dd40782806fc790aa00603ec1e573c393
Maksym <likeran2@gmail.com>
2019-03-01 00:19:50 +0200

    4 task solved

---
+++ js/jsN4.js
@@ -0,0 +1,23 @@
+document.querySelector(".calc-sum").addEventListener("click", calculate);
+
+class SumOfRange {
+	constructor() {
+  	this.sum = 0;
+    this.nextValue = 0;
+    this.resultSum = document.querySelector(".result-sum");
+  	this.rangeStart = parseInt(document.querySelector(".range-start").value);
+    this.rangeFinish = parseInt(document.querySelector(".range-finish").value);
+    this.countSumOfRange();
+  }
+	countSumOfRange() {
+  	while (this.rangeStart <= this.rangeFinish) {
+      this.sum = this.sum + this.rangeStart;
+      this.rangeStart++;
+    }
+    this.resultSum.textContent = this.sum;
+  }
+}
+
+function calculate() {
+	let newSum = new SumOfRange();
+}
