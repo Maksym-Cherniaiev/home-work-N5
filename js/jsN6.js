6a06b29ee39afa87181a08ffcfa18867f2b93e69
Maksym <likeran2@gmail.com>
2019-03-04 14:21:57 +0200

    minor changes added

--- js/jsN6.js
+++ js/jsN6.js
@@ -5,12 +5,6 @@ class Pyramid {
     this.rowCount = 0;
     this.result = document.querySelector(".pyramid");
     this.height = parseInt(document.querySelector(".pyramid-height").value);
-    this.showMessage();
-  }
-
-  showMessage() {
-    this.pyramidMessage = document.querySelector(".pyramid-message");
-    this.pyramidMessage.classList.add("piramid-message-visible");
   }
 }
 
@@ -23,7 +17,8 @@ class CountSpace extends Pyramid {
   createPyramid() {
     for (this.rowCount; this.rowCount < this.height; this.rowCount++) {
       this.newRow = document.createElement("p");
-      this.space = "_".repeat(this.height - this.rowCount);
+      this.newRow.classList.add("white-space");
+      this.space = "  ".repeat(this.height - this.rowCount);
       this.sign = "#".repeat(this.rowCount * 2 + 1);
       this.newRow.textContent = this.space + this.sign;
       this.result.appendChild(this.newRow);
