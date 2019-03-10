baf71afac763beb6385781153c04d4669df95799
Maksym <likeran2@gmail.com>
2019-03-10 21:19:25 +0200

    fetch-return problem resolved

--- js/jsN1.js
+++ js/jsN1.js
@@ -1,19 +1,48 @@
-document.querySelector(".executeOperation").addEventListener("click", convert)
+document.querySelector(".execute-operation").addEventListener("click", convert)
+const CONVERTER_API = "https://api.exchangeratesapi.io/latest?base=USD"
 
-class Converter {
-	constructor () {
-  	this.amount = document.querySelector(".converter").value;
-    this.result = document.querySelector(".convetionResult");
-    this.usdExchange = 8;
-    this.convertToUah();
+class CurrencyConverter {
+  convert(currentRate, userValue) {
+    return userValue * currentRate;
   }
-  
-  convertToUah() {
-  	let uahAmount = this.amount * this.usdExchange;
-    this.result.textContent = uahAmount;
+
+  async getCurrencyRate() {
+    const response = await fetch(CONVERTER_API);
+    const currencyData = await response.json();
+    return this.getEuroRate(currencyData);
+  }
+
+  getEuroRate(data) {
+    const euroRate = data.rates.EUR;
+    return euroRate;
+  }
+}
+
+
+class PageConverter extends CurrencyConverter {
+  constructor() {
+    super();
+    this.convertionResult = document.querySelector(".convetion-result");
+    this.convertUserValue();
+  }
+
+  getUserValue() {
+    return parseInt(document.querySelector(".user-value").value);
+  }
+
+  async convertUserValue() {
+    const userValue = this.getUserValue();
+    const currentEuroRate = await this.getCurrencyRate();
+
+    const convertedValue = this.convert(userValue, currentEuroRate);
+    this.showToUser(convertedValue);
+  }
+
+  showToUser(element) {
+    this.convertionResult.textContent = element;
   }
 }
 
 function convert() {
-	let exchangeOperation = new Converter();
+  new PageConverter();
 }
